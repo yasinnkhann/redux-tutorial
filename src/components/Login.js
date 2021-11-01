de48bd7aff550354f9e92025d64daa099f055455
Yasin Khan <yasinkhan500@gmail.com>
2021-10-31 17:25:54 -0700

    made redux tut into seperate repo

---
+++ src/components/Login.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { login, logout } from '../reducers/user';
+
+function Login() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(
+            login({ name: 'Yasin', age: 23, email: 'yasinkhan500@gmail.com' })
+          );
+        }}
+      >
+        Login
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        LOGOUT
+      </button>
+    </div>
+  );
+}
+
+export default Login;
